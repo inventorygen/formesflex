0a61c05660ced73528c96ca9af57a61113e927f3
inventorygen <jasmeshospice@gmail.com>
2026-01-09 17:23:41 +0100

    Update APPS_SCRIPT_URL to new endpoint

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const GOOGLE_CLIENT_ID =
   "246394466896-p3traebi4pl7jr8bph9jufqe1m4ic45g.apps.googleusercontent.com";
 
 export const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyfGNon7eyBF4DoGojNla8v1ga16QX9KvUACcenk_9TgUzyyRONS2gZfZdaSJ7-jXER/exec";
+  "https://script.google.com/macros/s/AKfycbxgaTnKG1AlzG8XgwZHP5Zy-y1LWRS4MzXIVSbaOFgqJsaUoaB74iQ9u9zLJxLAErGJ/exec";
