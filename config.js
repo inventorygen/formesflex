b2a385ce49f8dc6483fc295d12537e990edbf2d1
inventorygen <jasmeshospice@gmail.com>
2026-01-09 16:24:24 +0100

    Add Google API configuration constants

---
+++ config.js
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID =
+  "246394466896-p3traebi4pl7jr8bph9jufqe1m4ic45g.apps.googleusercontent.com";
+
+export const APPS_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbyfGNon7eyBF4DoGojNla8v1ga16QX9KvUACcenk_9TgUzyyRONS2gZfZdaSJ7-jXER/exec";
