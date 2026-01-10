b6c0d1eb5ae74bfaed8bbd9ad624e653307d4b0d
inventorygen <jasmeshospice@gmail.com>
2026-01-10 08:22:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/NOM_REPO/",
+  base: "/formesflex/",
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "Point Journaliere",
-        short_name: "PJ",
-        start_url: "/NOM_REPO/",
+        name: "GesFlex Forms - Point Journaliere",
+        short_name: "GesFlex",
+        start_url: "/formesflex/",
+        scope: "/formesflex/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#0b63f6",
